86f99ead72d2f91b26de528bb49c09d0342b1624
Peter Kelly <peter.kelly@penske.com>
2025-05-20 16:35:59 -0400

    Changed py file to a notebook and got the first iteration of the model up and running

--- leadgenerationv1.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-##use the leadgendataset.csv data for this
-##Try to make some kind of ML model that can predict if a customer has a rentalnet account or a salesforce account
-##Also try some feature engineering and try to tease out what might be important
